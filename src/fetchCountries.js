731249dbbdfb114447dc13bb982cc5fb68eabf6c
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-05-05 13:51:32 +0200

    task_updat

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,7 @@
 export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(resp => {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(resp => {
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
